fa782a9bb21e3ebf7723909e16a46b9656970a79
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2025-02-11 11:31:43 +0100

    add ex 1 2 and 3

--- HomeWork/week1/Week1.java
+++ HomeWork/week1/Week1.java
@@ -2,12 +2,18 @@ public class Week1
 {
 
 	public int factorial(int number){
-		if (number <= 2){
-			return number;
+		if (number == 1 || number == 0){
+			return 1;
 		}
 
 		return number * factorial(number- 1);
 
+	}
+
+
+	public static void main(String[] args){
+		Week1 week1 = new Week1();
+		System.out.println("Factorial of 7: " + week1.factorial(7));
 	}
 
 }
